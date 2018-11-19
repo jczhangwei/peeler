6115f3e94349ccd291f8c89e1e3c21838fdba999
zhangwei <jczhangwei@foxmail.com>
2018-11-20 02:34:24 +0800

    fix

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const { dialog, app, BrowserWindow, Menu, ipcMain } = require('electron');
 let win;
 function createWindow() {
     // 创建浏览器窗口
-    win = new BrowserWindow({width: 800, height: 600, webPreferences: { webSecurity: false }});
+    win = new BrowserWindow({width: 800, height: 600});
     win.maximize();
 
     // 然后加载应用的 index.html。
-    // win.loadFile('index.html');
-    win.loadURL('app://index.html');
+    win.loadFile('index.html');
 
     // 打开开发者工具
     win.webContents.openDevTools();
--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@ $(document).ready(function() {
     let myDocker = new wcDocker('.dockerContainer', {
         allowDrawers: true,
         responseRate: 10,
-        themePath: 'Build/Themes',
-        theme: 'default.min'
+        themePath   : 'node_modules/wcDocker/Build/Themes',
+        theme       : 'shadow.min'
     });
     console.log("啦啦啦啦");
     myDocker.registerPanelType('Info Panel', {
         isPrivate: true,
-        onCreate: function(myPanel) {
+        onCreate : function(myPanel) {
             myPanel.initSize(400, 200);
-            var $infoText = $('<span class="info" style="margin:20px;"></span>');
-            var $okButton = $('<button>OK</button>');
+            let $infoText = $('<span class="info" style="margin:20px;"></span>');
+            let $okButton = $('<button>OK</button>');
 
             myPanel.layout().addItem($infoText).stretch('100%', '100%');
             myPanel.layout().addItem($okButton, 0, 1).stretch('100%', '1%').css('text-align', 'right');
@@ -21,4 +21,7 @@ $(document).ready(function() {
             });
         },
     });
+
+    let infoPanel = myDocker.addPanel('Info Panel', wcDocker.DOCK.MODAL, null);
+    infoPanel.layout().scene().find('span').text("啦啦啦啦啦");
 });
